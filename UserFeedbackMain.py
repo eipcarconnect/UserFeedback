5873a1d32b7e55dc175c98dfd3cf919a05b554d0
SEUB <seb.le.gall@free.fr>
2020-07-06 20:11:14 +0200

    post handle done

--- UserFeedbackMain.py
+++ UserFeedbackMain.py
@@ -8,7 +8,8 @@ class UserFeedback(object):
     def __init__(self):
         if not os.path.isfile(FORM_PATH):
             with open(FORM_PATH, 'w') as f:
-                f.write('\t'.join(FEEDBACK_TABLE))
+                f.write('\t'.join(FEEDBACK_TABLE) + '\n')
+                f.flush()
 
         self.form = open(FORM_PATH, 'a')
 
@@ -18,24 +19,30 @@ class UserFeedback(object):
         if cherrypy.request.method == "POST":
             return self.handle_post(cherrypy.request)
 
-        return '''<meta http-equiv="refresh" content="durée;URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ">'''
+        return '''<meta http-equiv="refresh" content=";URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ">'''
 
     @cherrypy.expose
     def generate(self):
         pass
 
+    @cherrypy.tools.json_out()
+    def handle_post(self, request):
+        body = request.json
+
+        if not all(elem in FEEDBACK_TABLE for elem in body):
+            raise cherrypy.HTTPError(400, "Bad Request")
+
+        values = [str(body[key]) for key in FEEDBACK_TABLE]
+        print(values)
+        self.form.write('\t'.join(values) + '\n')
+        self.form.flush()
+
     # TO HANDLE A NEW URL CHANGE "urltest"
     @cherrypy.expose
     @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
     def urltest(self):
         print("urltest")
         return {"message": "urltest"}
-
-    def handle_post(self, request):
-        body = request.json
-        print("post", body)
-        return {"type": "post"}
 
 
 if __name__ == '__main__':
